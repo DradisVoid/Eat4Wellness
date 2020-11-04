69e97809a521c394f32973354dfbeecd59bc6a7c
Daniel <danielarn1799@gmail.com>
2020-11-04 17:05:27 -0500

    Added __str__ functions to models

--- app/models.py
+++ app/models.py
@@ -6,29 +6,46 @@ class User(models.Model):
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.email
+
 
 class Member(User):
     member_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     coach = models.ForeignKey('Coach', on_delete=models.RESTRICT)
 
+    def __str__(self):
+        return self.member_name
+
 
 class Coach(User):
     coach_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.coach_name
+
 
 class Admin(User):
     admin_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.admin_name
 
 
 class Nutrient(models.Model):
     nutrient_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nutrient_name
+
 
 class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.ingredient_name
 
 
 class HealthProfile(models.Model):
@@ -42,11 +59,17 @@ class HealthProfile(models.Model):
     recommended_daily_nutrients = models.ManyToManyField(Nutrient, through='HealthProfileNutrient')
     average_daily_nutrients = models.ManyToManyField(Nutrient, through='HealthProfileNutrient')
 
+    def __str__(self):
+        return str(self.member) + " Health Profile"
+
 
 class FoodProduct(models.Model):
     product_name = models.CharField(max_length=50)
     ingredients = models.ManyToManyField(Ingredient)
     nutrients = models.ManyToManyField(Nutrient, through='FoodNutrient')
+
+    def __str__(self):
+        return self.product_name
 
 
 class Meal(models.Model):
@@ -57,12 +80,18 @@ class Meal(models.Model):
     class Meta:
         ordering = ['-timestamp']
 
+    def __str__(self):
+        return str(self.timestamp)
+
 
 class FoodNutrient(models.Model):
     nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
     food_product = models.ForeignKey(FoodProduct, on_delete=models.CASCADE)
     amount = models.PositiveIntegerField()
     unit = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.nutrient) + " (" + str(self.amount) + " " + self.unit + ") - Food Product"
 
 
 class HealthProfileNutrient(models.Model):
@@ -71,8 +100,14 @@ class HealthProfileNutrient(models.Model):
     amount = models.PositiveIntegerField()
     unit = models.CharField(max_length=50)
 
+    def __str__(self):
+        return str(self.nutrient) + " (" + str(self.amount) + " " + self.unit + ") - Health Profile"
+
 
 class MealFood(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     food_product = models.ForeignKey(FoodProduct, on_delete=models.CASCADE)
     servings = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.food_product) + " - " + str(self.servings) + " servings"
