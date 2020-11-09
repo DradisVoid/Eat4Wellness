5e4a4a075f70a19f651c42142598e533d6877948
Daniel <danielarn1799@gmail.com>
2020-11-08 19:01:01 -0500

    Added model registering

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(MemberCoach)
+admin.site.register(Nutrient)
+admin.site.register(Ingredient)
+admin.site.register(FoodProduct)
+admin.site.register(Meal)
+admin.site.register(FoodNutrient)
+admin.site.register(HealthProfileNutrient)
+admin.site.register(HealthProfile)
+admin.site.register(MealFood)
