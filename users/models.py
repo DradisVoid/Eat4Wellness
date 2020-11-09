6cdc7f1ef386496121cc2bb7f8591363a62979b4
Daniel <danielarn1799@gmail.com>
2020-11-08 22:09:58 -0500

    Added __str__ functions

--- users/models.py
+++ users/models.py
@@ -6,11 +6,20 @@ from django.conf import settings
 class Admin(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "Admin: " + str(self.user)
+
 
 class Coach(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Coach: " + str(self.user)
 
 
 class Member(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     coach = models.ForeignKey(Coach, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return "Member: " + str(self.user)
