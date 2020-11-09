9fc716a0f4ecf89acd45abe0126500869031a2db
Daniel <danielarn1799@gmail.com>
2020-11-08 22:06:37 -0500

    Added Admin user role

--- users/models.py
+++ users/models.py
@@ -3,6 +3,10 @@ from django.conf import settings
 
 
 # Create your models here.
+class Admin(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
 class Coach(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
