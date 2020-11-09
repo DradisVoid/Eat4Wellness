0ca39d7750861487cdab5ed11017a640375c9de3
Daniel <danielarn1799@gmail.com>
2020-11-08 20:58:57 -0500

    Updated registers

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(MemberCoach)
+admin.site.register(Coach)
+admin.site.register(Member)
 admin.site.register(Nutrient)
 admin.site.register(Ingredient)
 admin.site.register(FoodProduct)
